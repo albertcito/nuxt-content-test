4c40e7b18eb84f4c96c4c9d877970799073e1566
albert <albert@gradguard.com>
2025-07-20 06:31:53 -0400

    test

--- pages/articles/index.vue
+++ pages/articles/index.vue
@@ -1,20 +1,23 @@
 <script setup lang="ts">
-const { data: articles } =  useAsyncData(
-  'all',
-  () => {
-    return queryCollection("all").where("type", "IN", ["article"]).all();
-  }
-);
+const pages = [
+  { id: 1, title: 'Page 1', path: '/articles/page-1' },
+  { id: 2, title: 'Page 2', path: '/articles/page-2/hola' },
+  { id: 3, title: 'Page 3', path: '/articles/page-3' },
+  { id: 4, title: 'Page 4', path: '/articles/page-4/mpre-hola' },
+  { id: 5, title: 'Page 5', path: '/articles/page-5' },
+  { id: 6, title: 'Page 6', path: '/articles/page-6' },
+  { id: 7, title: 'Page 7', path: '/articles/page-7' },
+];
 </script>
 
 <template>
   <NuxtLink
-        v-for="(article, index) in articles"
-        :key="article.id"
-        :to="article.path"
+        v-for="(page, index) in pages"
+        :key="page.id"
+        :to="page.path"
       >
         <div>
-          {{ article.title }}
+          {{ page.title }}
         </div>
   </NuxtLink>
 </template>
