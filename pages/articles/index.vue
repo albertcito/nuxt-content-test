fef09d881e37aa8ef052af850aac7f7991cdc62f
albert <albert@gradguard.com>
2025-07-20 14:59:47 -0400

    test N

--- pages/articles/index.vue
+++ pages/articles/index.vue
@@ -1,23 +1,9 @@
-<script setup lang="ts">
-const pages = [
-  { id: 1, title: 'Page 1', path: '/articles/page-1' },
-  { id: 2, title: 'Page 2', path: '/articles/page-2/hola' },
-  { id: 3, title: 'Page 3', path: '/articles/page-3' },
-  { id: 4, title: 'Page 4', path: '/articles/page-4/mpre-hola' },
-  { id: 5, title: 'Page 5', path: '/articles/page-5' },
-  { id: 6, title: 'Page 6', path: '/articles/page-6' },
-  { id: 7, title: 'Page 7', path: '/articles/page-7' },
-];
-</script>
-
 <template>
-  <NuxtLink
-        v-for="(page, index) in pages"
-        :key="page.id"
-        :to="page.path"
-      >
-        <div>
-          {{ page.title }}
-        </div>
-  </NuxtLink>
+  <NuxtLink to="/articles/page-1">Page 1</NuxtLink>
+  <NuxtLink to="/articles/page-2/hola">Page 2</NuxtLink>
+  <NuxtLink to="/articles/page-3">Page 3</NuxtLink>
+  <NuxtLink to="/articles/page-4/mpre-hola">Page 4</NuxtLink>
+  <NuxtLink to="/articles/page-5">Page 5</NuxtLink>
+  <NuxtLink to="/articles/page-6">Page 6</NuxtLink>
+  <NuxtLink to="/articles/page-7">Page 7</NuxtLink>
 </template>
